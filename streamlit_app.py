445c439a7262f5e3ca67c405ff9a1a53e0571ea7
shampien <30502060+MrMachine94@users.noreply.github.com>
2022-03-02 11:29:31 +0800

    Update with url data hyperlink (Update #1)

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,23 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title("Hello World!")
+DATA_URL = (
+"https://raw.githubusercontent.com/MrMachine94/streamlit/main/Motor_Vehicle_Collisions_-_Crashes.csv"
+)
+st.title("Motor Vehicle Collisions in New York City")
+st.markdown("This application is a Streamlit Dashboard that can be used "
+"to analyze motor vehicle collisions in NYC ")
+
+@st.cache(persist = True)
+def load_data(nrows) :
+    data = pd.read_csv(DATA_URL, nrows = nrows, parse_dates = [["CRASH_DATE", "CRASH_TIME"]])
+    data.dropna(subset = ["LATITUDE", "LONGITUDE"], inplace = True)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis = "columns", inplace = True)
+    data.rename(columns={"crash_date_crash_time" : "date/time"}, inplace = True)
+    return data
+
+data = load_data(100000)
+
+st.subheader("Raw Data")
+st.write(data)
