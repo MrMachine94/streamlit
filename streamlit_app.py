ffb7f6d75c38a9083af26ad75c1617ee59c7f21d
shampien <30502060+MrMachine94@users.noreply.github.com>
2022-03-03 13:28:59 +0800

    Debugging #1

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pydeck as pdk
-import plotly.express as px
+#import plotly.express as px
 
 DATA_URL = (
 "https://raw.githubusercontent.com/MrMachine94/streamlit/main/Motor_Vehicle_Collisions_-_Crashes.csv"
